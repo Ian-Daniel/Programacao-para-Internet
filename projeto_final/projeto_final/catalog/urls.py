8eb8d320ace85a3b8a918eaa6d1f7392a12f35c4
Ian-Daniel <iandaniel.vm@gmail.com>
2023-12-26 14:13:32 -0300

    Continuação do projeto final
    Fiz o formulário para criar, editar ou apagar treinador e jogadores, além de criar os determinados caminhos para URL dentro de cada redirecionamento.

--- projeto_final/projeto_final/catalog/urls.py
+++ projeto_final/projeto_final/catalog/urls.py
@@ -7,5 +7,17 @@ urlpatterns = [
     path('jogadores/', views.JogadorListView.as_view(), name='jogadores'),
     path('jogador/<int:pk>', views.JogadorDetailView.as_view(), name='jogador-detail'),
     path('treinador/', views.TreinadorListView.as_view(), name='treinador'),
-    path('treinador/<int:pk>', views.TreinadorDetailView.as_view(), name='treinador-detail')
+    path('treinador/<int:pk>', views.TreinadorDetailView.as_view(), name='treinador-detail'),
+]
+
+urlpatterns += [
+    path('treinador/create/', views.TreinadorCreate.as_view(), name='treinador_create'),
+    path('treinador/<int:pk>/update/', views.TreinadorUpdate.as_view(), name='treinador_update'),
+    path('treinador/<int:pk>/delete/', views.TreinadorDelete.as_view(), name='treinador_delete'),
+]
+
+urlpatterns += [
+    path('jogador/create/', views.JogadorCreate.as_view(), name='jogador_create'),
+    path('jogador/<int:pk>/update/', views.JogadorUpdate.as_view(), name='jogador_update'),
+    path('jogador/<int:pk>/delete/', views.JogadorDelete.as_view(), name='jogador_delete'),
 ]
--- projeto_final/projeto_final/catalog/views.py
+++ projeto_final/projeto_final/catalog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from catalog.models import Posicao, Jogador, Partida, Treinador
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Treinador
 
 # Create your views here.
 def index(request):
@@ -33,3 +37,27 @@ class TreinadorListView(generic.ListView):
     
 class TreinadorDetailView(generic.DetailView):
     model=Treinador
+
+class TreinadorCreate(CreateView):
+    model = Treinador
+    fields = '__all__'
+
+class TreinadorUpdate(UpdateView):
+    model = Treinador
+    fields = ['nome', 'idade', 'email']
+
+class TreinadorDelete(DeleteView):
+    model = Treinador
+    success_url = reverse_lazy('treinadores')
+    
+class JogadorCreate(CreateView):
+    model = Jogador
+    fields = '__all__'
+
+class JogadorUpdate(UpdateView):
+    model = Jogador
+    fields = ['nome', 'treinador', 'numeroDaCamisa', 'posicao']
+
+class JogadorDelete(DeleteView):
+    model = Jogador
+    success_url = reverse_lazy('jogadores')
